2539d234475460e0f1e2c145772cc4c7b68e77cd
Jon <jderbyshire67@gmail.com>
2022-05-20 13:22:19 +0100

    JD First question

--- questions/python1.py
+++ questions/python1.py
@@ -36,8 +36,17 @@
 	# What was the name of the function we have seen to check the length of a container?  Use your CLI to access the Python documentation and get help(len).
 
 def one(input1, input2):
-	return ""
-
+	if len(input1) > len(input2):
+		print(input1)
+		return input1
+		
+	elif len(input1) < len(input2):
+		print(input2)
+		return input2
+	else:
+		print(input1 + " " + input2)
+		return input1 + " " + input2
+one("harry", "paul")
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
 	# <QUESTION 2>
